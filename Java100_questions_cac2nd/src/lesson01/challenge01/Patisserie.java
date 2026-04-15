a5f121ef72a1cd07fa15cf660ae32f869aee9b24
tfukasa <tfukasa@C010423523.zipangu.cac.co.jp>
2026-04-15 09:58:42 +0900

    lesson01/challenge01 完了

--- Java100_questions_cac2nd/src/lesson01/challenge01/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge01/Patisserie.java
@@ -15,5 +15,12 @@
 package lesson01.challenge01;
 
 public class Patisserie {
+	public static void main(String[] args) {
+
+		System.out.println("たいへんお待たせしました。");
+		System.out.println("【ポエール・ネルメ】");
+		System.out.println("ただいまより開店です！！");
+
+	}
 
 }
