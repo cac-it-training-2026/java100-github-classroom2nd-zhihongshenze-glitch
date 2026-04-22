fb581f910419f9410c36580c64acdc5e1358b30a
tfukasa <tfukasa@C010423523.zipangu.cac.co.jp>
2026-04-22 17:04:44 +0900

    Lesson3 完了

--- Java100_questions_cac2nd/src/lesson03/challenge04/Explorer.java
+++ Java100_questions_cac2nd/src/lesson03/challenge04/Explorer.java
@@ -64,9 +64,22 @@ public class Explorer {
 		int fullLength = 300;
 		int member = 0;
 
-
 		//ここにwhile文を利用した処理を記述
+		while (member < 5) {
+			if (fullLength <= 0) {
+				member++;
+				fullLength = 300;
+			} else if (fullLength == 300) {
+				System.out.println("隊長：");
+				System.out.println((member + 1) + "人目が渡り始めたよ\n");
+				fullLength -= 50;
+			} else {
+				fullLength -= 50;
+				System.out.println("隊長：");
+				System.out.println("まだ渡ってるよ\n");
+			}
 
+		}
 
 		System.out.println("全員渡り終わったよ！");
 
