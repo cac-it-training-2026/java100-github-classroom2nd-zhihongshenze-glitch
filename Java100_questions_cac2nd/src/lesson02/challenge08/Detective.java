1aa73c79be7d0d668fa7b48d4110d5d3508c8c67
tfukasa <tfukasa@C010423523.zipangu.cac.co.jp>
2026-04-22 16:50:02 +0900

    Lesson2 完了

--- Java100_questions_cac2nd/src/lesson02/challenge08/Detective.java
+++ Java100_questions_cac2nd/src/lesson02/challenge08/Detective.java
@@ -47,7 +47,9 @@
 
 package lesson02.challenge08;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Detective {
 
@@ -67,11 +69,31 @@ public class Detective {
 		System.out.println("2．パターンＢ");
 		System.out.println("3．パターンＣ");
 		System.out.print("数字を選んでここに入力＞");
-
 		//ここに入力処理を入れる
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String str = reader.readLine();
+		int pattern = Integer.parseInt(str);
 
 		//ここにswitch文を入れる
+		switch (pattern) {
+		case 1:
+			System.out.println("\n探偵：");
+			System.out.println("やっぱりその経路が一番可能性が高いね。");
+			break;
+		case 2:
+			System.out.println("\n探偵：");
+			System.out.println("その可能性は低いかな。");
+			break;
+		case 3:
+			System.out.println("\n探偵：");
+			System.out.println("犬がよく知っている人物ならあり得るね。");
+			break;
+		default:
+			System.out.println("\n探偵：");
+			System.out.println("他に道はありませんよ。");
+			break;
 
+		}
 	}
 
 }
